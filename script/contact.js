64e37c01be7a33df83b4fcf63b17c406e68a9037
Toluwani Somade <Toluwani Somade>
2023-09-23 13:38:00 +0100

    Porfolio

---
+++ script/contact.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector('.lefta');
+    const icon = document.querySelector('.menu-icon');
+
+    menu.classList.toggle('active');
+    icon.classList.toggle('active');
+}
