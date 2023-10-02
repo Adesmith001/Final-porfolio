be1418f6cc785942868c051ce79a902924f8a89e
Toluwani Somade <Toluwani Somade>
2023-10-02 14:33:00 +0100

    Changes made: 1. Improved the Ui 2. Improved the functionalities

--- script/script.js
+++ script/script.js
@@ -5,3 +5,18 @@ function toggleMenu() {
     menu.classList.toggle('active');
     icon.classList.toggle('active');
 }
+
+const goToTopBtn = document.getElementById("goToTopBtn");
+
+window.onscroll = () => {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        goToTopBtn.style.display = "block";
+    } else {
+        goToTopBtn.style.display = "none";
+    }
+};
+
+goToTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+});
