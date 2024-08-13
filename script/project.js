88195f3ad83cb14127129b0e6f0ee44505a846b8
Toluwani Somade <Toluwani Somade>
2024-08-13 11:53:34 +0100

    Mobile responsiveness of home and project page

--- script/project.js
+++ script/project.js
@@ -8,3 +8,26 @@ $(".custom-carousel").owlCarousel({
       $(this).toggleClass("active");
     });
   });
+
+  function toggleMenu() {
+    const menu = document.querySelector('.lefta');
+    const icon = document.querySelector('.menu-icon');
+  
+    menu.classList.toggle('active');
+    icon.classList.toggle('active');
+  }
+  
+  const goToTopBtn = document.getElementById("goToTopBtn");
+  
+  window.onscroll = () => {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        goToTopBtn.style.display = "block";
+    } else {
+        goToTopBtn.style.display = "none";
+    }
+  };
+  
+  goToTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  });
